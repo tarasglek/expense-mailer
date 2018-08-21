c1bb4c5db8a61aef1d7a19b1a5428a217e734856
Taras Glek <github@glek.net>
2018-08-20 20:08:57 -0700

    error reporting via email

--- process_incoming_email.py
+++ process_incoming_email.py
@@ -23,52 +23,64 @@ import expense_tracker
 import invoice_parser
 import re
 import json
+import traceback
+
 # https://github.com/olucurious/PyFCM/issues/115
 from requests_toolbelt.adapters import appengine
 appengine.monkeypatch()
 
 class LogSenderHandler(InboundMailHandler):
     def receive(self, mail_message):
-        def reply(msg):
-            mail.send_mail(sender=mail_message.to,
-                to=mail_message.sender,
-                subject='Re: ' + mail_message.original['Subject'],
-                body=msg,
-                headers={
+        def reply(msg, attachments=None):
+            args = {
+                'sender': mail_message.to,
+                'to': mail_message.sender,
+                'subject': 'Re: ' + mail_message.original['Subject'],
+                'body': msg,
+                'headers': {
                     "References": mail_message.original.get('References', ''),
                     "In-Reply-To": mail_message.original.get('Message-ID', ''),
                 },
-            )
-        m = re.search(r"(\S+)@", mail_message.to)
-        if not m:
-            return
-        config = json.load(open('sheets.json'))
-        valid_user = False
-        for u in config["users"]:
-            valid_user = valid_user or u in mail_message.sender
-        if not valid_user:
-            reply("Invalid user " + mail_message.sender)
-            return
-        expense_category = m.groups()[0]
-        email = ParsedMessage(mail_message.original)
-        parsed = invoice_parser.parse_msg(email.above_fwd_text)
-        if not parsed:
-            reply("Could not parse invoice")
-            return
-        sheet_link = config["sheets"][expense_category]
-        folder_id = config["folders"][expense_category]
-        hash = email.hash()
-        links = {}
-        for (filename, body) in email.attachments():
-            link = expense_tracker.upload_file(folder_id, hash + '.' + filename, body)
-            links[filename] = link
-        parsed['contractor'] = email.fwd_headers['From']
-        parsed['date'] = email.fwd_headers['Date']
-        parsed['hash'] = hash
-        expense_tracker.add_entry(sheet_link, links, parsed)
-        reply("""
-                Added {} to
-                {}
-            """.format(str(parsed), sheet_link))
+            }
+            if attachments:
+                args['attachments'] = attachments
+            mail.send_mail(**args)
+        def process():
+            m = re.search(r"(\S+)@", mail_message.to)
+            if not m:
+                return
+            config = json.load(open('sheets.json'))
+            valid_user = False
+            for u in config["users"]:
+                valid_user = valid_user or u in mail_message.sender
+            if not valid_user:
+                reply("Invalid user " + mail_message.sender)
+                return
+            expense_category = m.groups()[0]
+            email = ParsedMessage(mail_message.original)
+            parsed = invoice_parser.parse_msg(email.above_fwd_text)
+            if not parsed:
+                reply("Could not parse invoice")
+                return
+            sheet_link = config["sheets"][expense_category]
+            folder_id = config["folders"][expense_category]
+            hash = email.hash()
+            links = {}
+            for (filename, body) in email.attachments():
+                link = expense_tracker.upload_file(folder_id, hash + '.' + filename, body)
+                links[filename] = link
+            parsed['contractor'] = email.fwd_headers['From']
+            parsed['date'] = email.fwd_headers['Date']
+            parsed['hash'] = hash
+            expense_tracker.add_entry(sheet_link, links, parsed)
+            reply("""
+                    Added {} to
+                    {}
+                """.format(str(parsed), sheet_link))
+        try:
+            process()
+        except Exception:
+            just_the_string = traceback.format_exc()
+            reply(just_the_string, attachments=[("orig_msg.txt", mail_message.original.as_string())])
 
 app = webapp2.WSGIApplication([LogSenderHandler.mapping()], debug=True)
